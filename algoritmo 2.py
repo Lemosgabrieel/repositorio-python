2aedb3fc1ba36031a7f88c4ce26ac018ca7b0c4a
Gabriel Lemos da costa Gonçalves de Oliveira <biellemosgon@hotmail.com>
2025-07-28 03:24:30 -0300

    Algoritmo de calculo do estoque medio
    Faça um algoritmo para calcular o estoque médio de uma peça, sendo que: 
    estoque médio = (quantidade minima + quantidade máxima) / 2

---
+++ algoritmo 2.py
@@ -0,0 +1,6 @@
+quantidade_minima = input("Digite a quantidade mínima de itens: ")
+quantidade_maxima = input("Digite a quantidade máxima de itens: ")
+
+estoque_medio = (int(quantidade_minima) + int(quantidade_maxima)) / 2
+
+print("A quantidade média de itens no estoque é:", estoque_medio)
