fb0d944fd6bb1ae11d20ca5800bacf77840df39c
Gabriel Lemos da costa Gonçalves de Oliveira <biellemosgon@hotmail.com>
2025-07-28 03:20:39 -0300

    Algoritmo de soma e multiplicação de números
    faça um algoritmo para somar dois números e multiplicar o resultado pelo primeiro número

---
+++ algoritmo 1.py
@@ -0,0 +1,12 @@
+#entradas 
+n1 =int(input("Digite o primeiro número: "))
+n2 =int(input("Digite o segundo número: "))
+
+#processamento
+soma = n1 + n2
+multiplicacao = soma * n2
+
+#saida 
+print("A soma é:", soma)
+print("A multiplicação é:", multiplicacao) 
+
